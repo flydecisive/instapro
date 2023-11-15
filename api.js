f9202783e9debe2bbba9242ee247ef2779bf76a1
Maksim <45427901+flydecisive@users.noreply.github.com>
2023-11-15 13:45:47 +0300

    интегрировал в верстку список постов из API

--- api.js
+++ api.js
@@ -1,15 +1,13 @@
 // Замени на свой, чтобы получить независимый от других набор данных.
 // "боевая" версия инстапро лежит в ключе prod
 const personalKey = "prod";
+// const personalKey = "flydecisive";
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
-export function getPosts({ token }) {
+export function getPosts() {
   return fetch(postsHost, {
     method: "GET",
-    headers: {
-      Authorization: token,
-    },
   })
     .then((response) => {
       if (response.status === 401) {
